3ab25da918573a2e420c26abf6f257d35fa048d6
vsiago <vsiago23@gmail.com>
2024-02-08 20:51:32 -0300

    Perfeito commit

---
+++ src/screens/Places.jsx
@@ -0,0 +1,20 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Places() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Places Screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 20,
+  },
+});
