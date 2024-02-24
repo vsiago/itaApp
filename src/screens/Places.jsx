eb295fc0b529d89cedf78f2c18b823a16c2b7de0
vsiago <vsiago23@gmail.com>
2024-02-24 00:13:29 -0300

    Botao adicionar adicionado

--- src/screens/Places.jsx
+++ src/screens/Places.jsx
@@ -4,7 +4,7 @@ import Swiper from 'react-native-swiper'
 
 export default function Feed() {
   return (
-    <ScrollView className="">
+    <ScrollView>
       <View className="flex-1 items-start  bg-[#D0DAE5]">
         <HeaderProfile />
         <View className=" w-[90] pb-1 absolute top-[66] ml-5 left-0 z-[2002] border-b-2 border-sky-500 rounded-br-2xl rounded blur-sm pl-2">
@@ -26,22 +26,22 @@ export default function Feed() {
             <View>
               <Image
                 className="rounded-2xl"
-                source={require("../../assets/hospital-sx.png")} // Substitua pelo caminho da sua imagem
-                style={{ width: "100%", height: "100%" }} // Ajuste os valores conforme necessário
+                source={require("../../assets/hospital-sx.png")}
+                style={{ width: "100%", height: "100%" }}
               />
             </View>
             <View>
               <Image
                 className="rounded-2xl"
-                source={require("../../assets/praca-texeira.png")} // Substitua pelo caminho da sua imagem
-                style={{ width: "100%", height: "100%" }} // Ajuste os valores conforme necessário
+                source={require("../../assets/praca-texeira.png")}
+                style={{ width: "100%", height: "100%" }}
               />
             </View>
             <View>
               <Image
                 className="rounded-2xl"
-                source={require("../../assets/famacia-central.png")} // Substitua pelo caminho da sua imagem
-                style={{ width: "100%", height: "100%" }} // Ajuste os valores conforme necessário
+                source={require("../../assets/famacia-central.png")}
+                style={{ width: "100%", height: "100%" }}
               />
             </View>
           </Swiper>
@@ -49,12 +49,12 @@ export default function Feed() {
 
         <View className="flex-1 w-full items-start justify-start px-5">
 
-          {/* ------------------- Menu Servicos ------------------- */}
+          {/* ------------------- Menu Lugares ------------------- */}
 
           <View className="absolute -top-8 flex-row gap-2 justify-center items-center pr-1 left-0 right-0 z-50">
             <TouchableOpacity className="bg-white h-24 w-28 rounded-lg items-center justify-center">
-              <Text className="text-xs font-medium text-slate-600">
-                A Prefeitura
+              <Text className="text-xs font-medium text-slate-600 break-words text-center">
+                Unidades de{'\n'}Saude
               </Text>
             </TouchableOpacity>
             <TouchableOpacity className="bg-white h-24 w-28 rounded-lg items-center justify-center">
@@ -63,8 +63,8 @@ export default function Feed() {
               </Text>
             </TouchableOpacity>
             <TouchableOpacity className="bg-white h-24 w-28 rounded-lg items-center justify-center">
-              <Text className="text-xs font-medium text-slate-600">
-                Carta de Servicos
+              <Text className="text-xs font-medium text-slate-600 text-center">
+                Carta de{'\n'}Servicos
               </Text>
             </TouchableOpacity>
           </View>
@@ -72,18 +72,22 @@ export default function Feed() {
 
           <View className="flex-1 justify-start items-start w-full mt-24 mb-10">
             <Text className="uppercase font-medium tracking-wide text-slate-600 mb-5">Noticias</Text>
-            <TouchableOpacity style={{ elevation: 1 }} className="w-full bg-slate-200 rounded-bl-md rounded-md mb-4">
+            <TouchableOpacity style={{ elevation: 1 }} className="w-full bg-slate-100 rounded-bl-md rounded-md mb-4">
               <View className="bg-slate-400 h-[160] w-full items-center justify-center rounded-t-md">
-                <Text>Imagem</Text>
+                <Text className="text-slate-500 text-xl border-2 border-slate-500 p-5 py-4 rounded-xl">Imagem</Text>
               </View>
               <View className="">
-                <Text className="text-lg font-bold text-slate-600 mx-3 my-3 mb-1 leading-6">Novo Hospital Sao Francisco Xavier</Text>
+                <Text className="text-lg font-bold text-slate-600 mx-3 my-6 mb-1 leading-6">Novo Hospital Sao Francisco Xavier</Text>
               </View>
-              <View className="px-3 pb-3">
+              <View className="px-3 pb-4">
                 <Text className="text-base font-normal text-slate-500">Aqui vai uma descricao de um materia da Prefeitura para o publico clicar e consumir.</Text>
               </View>
+              <View className="mx-3 mb-3 pt-2 border-t-2 border-slate-200 flex-row">
+                <Text className=" py-1 font-semibold text-slate-600 text-base mr-3">Curtir   |</Text>
+                <Text className=" py-1 font-semibold text-slate-500 text-base">96</Text>
+              </View>
             </TouchableOpacity>
-            <TouchableOpacity style={{ elevation: 1 }} className="w-full bg-slate-200 rounded-bl-md rounded-md mb-4">
+            <TouchableOpacity style={{ elevation: 1 }} className="w-full bg-slate-100 rounded-bl-md rounded-md mb-4">
               <View className="bg-slate-400 h-[160] w-full items-center justify-center rounded-t-md">
                 <Text>Imagem</Text>
               </View>
@@ -94,7 +98,7 @@ export default function Feed() {
                 <Text className="text-base font-normal text-slate-500">Aqui vai uma descricao de um materia da Prefeitura para o publico clicar e consumir.</Text>
               </View>
             </TouchableOpacity>
-            <TouchableOpacity style={{ elevation: 1 }} className="w-full bg-slate-200 rounded-bl-md rounded-md mb-4">
+            <TouchableOpacity style={{ elevation: 1 }} className="w-full bg-slate-100 rounded-bl-md rounded-md mb-4">
               <View className="bg-slate-400 h-[160] w-full items-center justify-center rounded-t-md">
                 <Text>Imagem</Text>
               </View>
