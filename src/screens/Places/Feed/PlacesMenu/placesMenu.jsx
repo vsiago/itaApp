feaa748b80350266041b4c0f55f2682f6573515f
vsiago <vsiago22@gmail.com>
2024-02-26 16:56:01 -0300

    Imagens posts timeline iniciado

--- src/screens/Places/Feed/PlacesMenu/placesMenu.jsx
+++ src/screens/Places/Feed/PlacesMenu/placesMenu.jsx
@@ -111,18 +111,38 @@ export default function PlacesMenu() {
         {/* Renderizar os posts */}
         {selectedCategory
           ? selectedCategory.posts.map((post, index) => (
-              <View key={index}>
-                <Text>{post.nome}</Text>
-                {/* Renderizar outros detalhes do post conforme necessário */}
-              </View>
+            <TouchableOpacity key={index} className="w-full rounded-lg bg-white mb-4">
+            {/* <Image
+              source={{ uri: imageURL }}
+              style={{ height: 300, width: '100%', resizeMode: 'cover' }}
+              className="rounded-t-lg"
+            /> */}
+            <View className="">
+              <Text className="text-lg font-bold text-slate-600 mx-3 my-3 mb-1 leading-6">{post.nome}</Text>
+            </View>
+            <View className="mx-3 mt-2 mb-3 pt-2 border-t-2 border-slate-200 flex-row">
+              <Text className=" py-1 font-semibold text-slate-600 text-base mr-3">Curtir   |</Text>
+              <Text className=" py-1 font-semibold text-slate-500 text-base">{post.likes}</Text>
+            </View>
+          </TouchableOpacity>
             ))
           : selectedPlace &&
             selectedPlace.categoria.map((categoria, index) =>
               categoria.posts.map((post, index) => (
-                <View key={index}>
-                  <Text>{post.nome}</Text>
-                  {/* Renderizar outros detalhes do post conforme necessário */}
+                <TouchableOpacity key={index} className="w-full rounded-lg bg-white mb-4">
+                {/* <Image
+                  source={{ uri: imageURL }}
+                  style={{ height: 300, width: '100%', resizeMode: 'cover' }}
+                  className="rounded-t-lg"
+                /> */}
+                <View className="">
+                  <Text className="text-lg font-bold text-slate-600 mx-3 my-3 mb-1 leading-6">{post.nome}</Text>
                 </View>
+                <View className="mx-3 mt-2 mb-3 pt-2 border-t-2 border-slate-200 flex-row">
+                  <Text className=" py-1 font-semibold text-slate-600 text-base mr-3">Curtir   |</Text>
+                  <Text className=" py-1 font-semibold text-slate-500 text-base">{post.likes}</Text>
+                </View>
+              </TouchableOpacity>
               ))
             )}
       </ScrollView>
