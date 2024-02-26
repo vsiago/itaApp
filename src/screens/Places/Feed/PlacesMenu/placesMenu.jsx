69598e72d60443120f19341483d20ca4697db40f
vsiago <vsiago23@gmail.com>
2024-02-25 23:27:39 -0300

    Comecando a consumir api

--- src/screens/Places/Feed/PlacesMenu/placesMenu.jsx
+++ src/screens/Places/Feed/PlacesMenu/placesMenu.jsx
@@ -1,20 +1,18 @@
-import { useNavigation } from '@react-navigation/native';
 import { View, Text, TouchableOpacity } from 'react-native'
 import { useState, useEffect } from 'react'
+import placesData from '../../../../../assets/places.json'
 
 export default function PlacesMenu() {
-  const navigation = useNavigation();
-
-  const navigateToDynamicScreen = (id) => {
-    navigation.navigate('todos', { id });
-  };
 
   const [colorButtonDefault, setColorButtonDefault] = useState('')
   const [todos, setTodos] = useState('api-todos')
+  const [places, setPlaces] = useState(null)
 
   useEffect(() => {
+    setPlaces(placesData.lugares)
     setTodos(todos)
     setColorButtonDefault('bg-slate-500')
+
   }, [])
   // colorButtonDefault
 
@@ -23,20 +21,8 @@ export default function PlacesMenu() {
 
     <View className="flex-1 w-full items-start justify-start px-5 absolute -top-10 left-5 z-50">
       <View className="flex-row gap-2 justify-center items-center pr-1">
-        {/* Botao Todos */}
-        <TouchableOpacity onPress={() => navigateToDynamicScreen('todos')} className="bg-white opacity-90 rounded-full items-center justify-center p-1" style={{ elevation: 2 }}>
-          <View style={{ elevation: 20 }} className={`${colorButtonDefault} rounded-full h-[75px] w-[75px] items-center justify-center`}>
-            <Text className="text-lg font-medium text-white break-words text-center">
-              Ico
-            </Text>
-          </View>
-          <View className="my-3">
-            <Text className="text-xs font-semibold text-slate-500 break-words text-center">
-              Todos
-            </Text>
-          </View>
-        </TouchableOpacity>
-        {/* <TouchableOpacity className="bg-white opacity-90 rounded-full items-center justify-center p-1" style={{ elevation: 2 }}>
+        {places.map((place, key) => (
+          <TouchableOpacity key={key} className="bg-white opacity-90 rounded-full items-center justify-center p-1" style={{ elevation: 2 }}>
             <View style={{ elevation: 20 }} className={`${colorButtonDefault} rounded-full h-[75px] w-[75px] items-center justify-center`}>
               <Text className="text-lg font-medium text-white break-words text-center">
                 Ico
@@ -44,34 +30,11 @@ export default function PlacesMenu() {
             </View>
             <View className="my-3">
               <Text className="text-xs font-semibold text-slate-500 break-words text-center">
-                Prefeitura
+                {place.nome}
               </Text>
             </View>
           </TouchableOpacity>
-          <TouchableOpacity className="bg-white opacity-90 rounded-full items-center justify-center p-1" style={{ elevation: 2 }}>
-            <View style={{ elevation: 20 }} className={`${colorButtonDefault} rounded-full h-[75px] w-[75px] items-center justify-center`}>
-              <Text className="text-lg font-medium text-white break-words text-center">
-                Ico
-              </Text>
-            </View>
-            <View className="my-3">
-              <Text className="text-xs font-semibold text-slate-500 break-words text-center">
-                Praia
-              </Text>
-            </View>
-          </TouchableOpacity>
-          <TouchableOpacity className="bg-white opacity-90 rounded-full items-center justify-center p-1" style={{ elevation: 2 }}>
-            <View style={{ elevation: 20 }} className={`${colorButtonDefault} rounded-full h-[75px] w-[75px] items-center justify-center`}>
-              <Text className="text-lg font-medium text-white break-words text-center">
-                Ico
-              </Text>
-            </View>
-            <View className="my-3">
-              <Text className="text-xs font-semibold text-slate-500 break-words text-center">
-                Serras
-              </Text>
-            </View>
-          </TouchableOpacity> */}
+        ))}
       </View>
     </View>
 
